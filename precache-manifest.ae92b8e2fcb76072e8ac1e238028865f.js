484a53c484675d5a94a8f6cbdb7d0597eaa7cb37
eshux <54838488+eshux@users.noreply.github.com>
2021-01-07 18:06:27 +0200

    deploy

---
+++ precache-manifest.ae92b8e2fcb76072e8ac1e238028865f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120c6fb36b7f4ef47117",
+    "url": "/todo/css/app.e3abc280.css"
+  },
+  {
+    "revision": "5571db3c36ee962fda26",
+    "url": "/todo/css/chunk-vendors.b0b28440.css"
+  },
+  {
+    "revision": "cb9a76b41ba8dea59605ddcc9507dc5a",
+    "url": "/todo/img/x.cb9a76b4.svg"
+  },
+  {
+    "revision": "ef885c56b1ab7dd5845a271cabb19bc1",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "120c6fb36b7f4ef47117",
+    "url": "/todo/js/app.b52bbb22.js"
+  },
+  {
+    "revision": "5571db3c36ee962fda26",
+    "url": "/todo/js/chunk-vendors.da7a4240.js"
+  },
+  {
+    "revision": "8d4192f82ff22aae2f49028b915656fa",
+    "url": "/todo/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/todo/robots.txt"
+  }
+]);
